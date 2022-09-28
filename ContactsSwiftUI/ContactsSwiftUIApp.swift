5fc853f7cc176b5982f18b4fab71f80750f66487
JohnyLearnSwift <eugenietyan@gmail.com>
2022-09-28 14:09:00 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Eugenie Tyan on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
